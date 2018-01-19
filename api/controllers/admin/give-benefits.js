be972ec17116c4bfd8afaf5fe1e94702506556d9
donalbertico <atates@udlanet.ec>
2018-01-19 09:39:30 -0500

    finishing some pivtoal histpries

---
+++ api/controllers/admin/give-benefits.js
@@ -0,0 +1,48 @@
+module.exports = {
+
+  friendlyName: 'give a user benefits',
+
+  description: 'give befits to a user from the product code on input',
+
+  inputs: {
+    user : {
+      description : 'user id to promote',
+      type : 'string',
+      required : true
+    },
+    code : {
+      description : 'product code',
+      type : 'string',
+      required : true
+    }
+  },
+
+  fn: function (inputs, exits,env) {
+    console.log('beneficiendo o lo q sea');
+    var  details = [{code:inputs.code}];
+    User.findOne(inputs.user,(err,found)=>{
+      if(err)exits.error(err);
+      User.applyBenefits(found.id,details,(err,type)=>{
+        if(err) exits.error(err);
+        sails.hooks.mail.send(
+                              'bonusBenefits',
+                              {
+                                type: type,
+                                recipientName : found.firstName
+                              },
+                              {
+                                to: found.email,
+                                subject: 'Regalo de Ecuador House Rentals',
+                              },
+                               function(err) {
+                                if(err) {
+                                  console.log(err);
+                                  return exits.error(err);
+                                };
+                                  return exits.success(err);
+                               }
+                             );
+      });
+    });
+  }
+};
