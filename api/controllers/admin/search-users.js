16a2b85268d8016ea774aa05a9353b14d5181964
donalbertico <atates@udlanet.ec>
2018-03-08 18:18:46 -0500

    adding user type

--- api/controllers/admin/search-users.js
+++ api/controllers/admin/search-users.js
@@ -12,6 +12,9 @@ function parseQuery(inputs){
   if (inputs.from && inputs.to) {
     query.createdAt = {'>' : inputs.from, '<' : inputs.to};
   }
+  if(inputs.type == 'n'){
+    query.collaborateWith = null;
+  }
   if(Object.keys(query).length<1){
     return null;
   }
@@ -42,6 +45,9 @@ module.exports = {
     },
     to :{
       type :'number'
+    },
+    type:{
+      type:'string'
     }
   },
 
@@ -60,18 +66,29 @@ module.exports = {
 
 
   fn: function (inputs, exits) {
-    console.log('searching',inputs);
-
     var query=parseQuery(inputs);
-    console.log(query);
     if(!query){
       query={};
     }
-    User.find(query).populate('properties').populate('passports').sort('createdAt DESC').exec((err,users)=>{
-      console.log(err);
-      if(err)return exits.error(err);
-      exits.success(users);
-    });
+    if(inputs.type == 'h' || inputs.type =='r' || || inputs.type =='e'){
+      var users = [];
+      Enterprise.find({type : inputs.type}).populate('collaborators').populate('deputy').exec((err,enterprises)=>{
+        if(err)return exits.error(err);
+        enterprises.forEach((item)=>{
+          users.push(item.deputy);
+          item.collaborators.forEach((collaborator)=>{
+            users.push(collaborator);
+          });
+        });
+        exits.success(users);
+      });
+    }else{
+      User.find(query).populate('properties').populate('passports').sort('createdAt DESC').exec((err,users)=>{
+        if(err)return exits.error(err);
+        exits.success(users);
+      });
+    }
+
   }
 
 
