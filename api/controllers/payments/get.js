e8cf12c84afa5db5f06df0026154970c6282123e
Juan Rosero <jfrosero@udlanet.ec>
2018-10-14 17:46:21 -0500

    Add product code to payments table

--- api/controllers/payments/get.js
+++ api/controllers/payments/get.js
@@ -19,6 +19,10 @@ module.exports = {
   fn: function (inputs, exits,env) {
     Payments.find({verified : false}).populate('payer').exec((err,found)=>{
       if(err) return exits.error(err);
+      found=found.map((payment)=>{
+        payment.productCode = payment.details[0].code;
+        return payment;
+      });
       return exits.success(found)
     });
   }
