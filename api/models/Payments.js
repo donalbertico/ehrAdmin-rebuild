f1258246746bdbc1e00825c38614d85977d7bb3f
Juan Rosero <jfrosero@udlanet.ec>
2018-11-27 10:22:10 -0500

    Account result count to requests view

--- api/models/Payments.js
+++ api/models/Payments.js
@@ -1,11 +1,12 @@
-var detailDic = require('./details');
-  /**
+/* globals Payments, User, Bill, sails */
+var detailDic = require('./details')
+/**
  * Payments.js
  *
  * @description :: A model definition.  Represents a database table/collection/etc.
  * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
  */
- /**
+/**
  *
  *Object describing a tax rate. Uses the standar defined by SRI.
  *More info on http://www.sri.gob.ec/DocumentosAlfrescoPortlet/descargar/fbb05cf5-32f2-4cc3-bc8a-b539c9e0ed74/FICHA+TECNICA+COMPROBANTES+ELECTRO%B4NICOS+offline.pdf
@@ -17,7 +18,7 @@ var detailDic = require('./details');
  *@property {!string} rateCode - SRI code used to identify tax rate
  *@property {!string} value - Value for the tax aleady applied to the cointaining Item
  */
- /**
+/**
  *
  *Object describing a single item inside a bill's Detail
  *@typedef {Object} Item
@@ -30,7 +31,7 @@ var detailDic = require('./details');
  *@property {number} total - Calculated value equal to qty*unitPrice - discount
  *@property {Tax[]} taxes - Array con2aining Taxes applied to the current item
  */
- /**
+/**
  *
  *An object describing the purchase.
  *@typedef {Object} Detail
@@ -43,63 +44,62 @@ var detailDic = require('./details');
 */
 module.exports = {
   attributes: {
-    payer:{model:'User',required:true},
-    bill:{model:'Bill'},
-    amount:{type:'number'},
-    type : {type: 'string'},
-    details:{
-      type:'json',
-      required:true
+    payer: { model: 'User', required: true },
+    bill: { model: 'Bill' },
+    amount: { type: 'number' },
+    type: { type: 'string' },
+    details: {
+      type: 'json',
+      required: true
     },
-    method:{type:'string',isIn:['PayPal','Deposito Bancario']},
-    verified:{type:'boolean',defaultsTo:false},
-    verifiedAt:{type:'string', columnType: 'date'},
-    transferNumber : {type: 'string',maxLength: 20, minLength : 1, defaultsTo:''},
-    invoiced : {type: 'boolean',defaultsTo : false}
+    method: { type: 'string', isIn: ['PayPal', 'Deposito Bancario'] },
+    verified: { type: 'boolean', defaultsTo: false },
+    verifiedAt: { type: 'string', columnType: 'date' },
+    transferNumber: { type: 'string', maxLength: 20, minLength: 1, defaultsTo: '' },
+    invoiced: { type: 'boolean', defaultsTo: false }
   },
-  beforeCreate : (newRecord,next)=> {
-    newRecord.details.forEach((detail,index)=>{
-      newRecord.details[index] = detailDic.detailFromCode(detail.code,detail.qty,detail.discount);
-    });
-    next();
+  beforeCreate: (newRecord, next) => {
+    newRecord.details.forEach((detail, index) => {
+      newRecord.details[index] = detailDic.detailFromCode(detail.code, detail.qty, detail.discount)
+    })
+    next()
   },
-  verify : (paymentId, cb) => {
-    Payments.findOne(paymentId).populate('payer').exec((err,payment)=>{
-        if(err) return cb(err);
-        User.applyBenefits(payment.payer.id,payment.details,(err,benefType)=>{
-          if(err) return cb(err);
-          var bill = {
-            payment:payment.id,
-            name:payment.payer.paymentInfo.name||'Consumidor Final',
-            identifier:payment.payer.paymentInfo.identifier||'9999999999999',
-            address:payment.payer.paymentInfo.address,
-            idType:payment.payer.paymentInfo.idType||'07',
-            email:payment.payer.email,
-            details : payment.details
-          };
-          Bill.create(bill).meta({fetch:true}).exec((err,newBill)=>{
-            if(err) return cb(err);
-            Payments.update(payment.id,{verifiedAt: new Date().getTime() ,verified:true,bill:newBill.id,amount:bill.total,type:benefType},(err)=>{
-              if(err) return cb(err);
-              sails.hooks.mail.send(
-                                    'paymentAproved',
-                                    {
-                                      recipientName: payment.payer.firstName,
-                                      type: benefType
-                                    },
-                                    {
-                                      to: payment.payer.email,
-                                      subject: 'Pago recibido'
-                                    },
-                                     function(err) {
-
-                                      if(err) return cb(err);
-                                      return cb();
-                                     }
-                                   );
-            });
-          });
-        });
-    });
+  verify: (paymentId, cb) => {
+    Payments.findOne(paymentId).populate('payer').exec((err, payment) => {
+      if (err) return cb(err)
+      User.applyBenefits(payment.payer.id, payment.details, (err, benefType) => {
+        if (err) return cb(err)
+        var bill = {
+          payment: payment.id,
+          name: payment.payer.paymentInfo.name || 'Consumidor Final',
+          identifier: payment.payer.paymentInfo.identifier || '9999999999999',
+          address: payment.payer.paymentInfo.address,
+          idType: payment.payer.paymentInfo.idType || '07',
+          email: payment.payer.email,
+          details: payment.details
+        }
+        Bill.create(bill).meta({ fetch: true }).exec((err, newBill) => {
+          if (err) return cb(err)
+          Payments.update(payment.id, { verifiedAt: new Date().getTime(), verified: true, bill: newBill.id, amount: bill.total, type: benefType }, (err) => {
+            if (err) return cb(err)
+            sails.hooks.mail.send(
+              'paymentAproved',
+              {
+                recipientName: payment.payer.firstName,
+                type: benefType
+              },
+              {
+                to: payment.payer.email,
+                subject: 'Pago recibido'
+              },
+              function (err) {
+                if (err) return cb(err)
+                return cb()
+              }
+            )
+          })
+        })
+      })
+    })
   }
-};
+}
